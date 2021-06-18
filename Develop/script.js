bff335c16c069e48398494a9287bd8961de4cf3f
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-06-18 16:30:18 -0400

    Added function to prompt user for password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); 
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +12,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Prompt user for password length
+function generatePassword(){
+  var passwordLength = prompt("Length of Password: ", "type a number between 8 and 128");
+  if (passwordLength >= 8 && passwordLength <= 128){
+    window.alert("Your password will be " + passwordLength + " characters long");
+  } else {
+    window.prompt("Invalid length.", "type a number between 8 and 128");
+  }
+}
