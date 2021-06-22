8440af85924492a4c128cb017511d06526b157fd
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-06-22 15:48:20 -0400

    Cleaned up code. Added a way to reset array so new password can be generated

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,7 @@ var alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
 var alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "[", "]", "}", ":", ";", "<", ">", ",", ".", "'", "?", "/"];
+var pwordChar = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -11,24 +12,27 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
+  // Reset pwordChar array so a new password can be generated
+  pwordChar = [];
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var pwordChar = [];
-
 // Prompt user for password length and generate password
 function generatePassword(){
   var passwordLength = prompt("Length of Password: ", "type a number between 8 and 128");
+  
   // Check if password is appropriate length
-  if (passwordLength <= 8 && passwordLength >= 128){
-    window.prompt("Invalid length.", "type a number between 8 and 128");
+  if (passwordLength < 8 && passwordLength > 128){
+    window.prompt("Invalid length.", "type a number between 8 and 128"); // this no longer works???
   } else {
     window.alert("Your password will be " + passwordLength + " characters long");
     promptCriteria();
-    var password = []
+    
+    // Create password based on user criteria
+    var password = [];
     for (var i=0; i < passwordLength; i++){
       var index = Math.floor(Math.random() * pwordChar.length); // double check this
       password[i] = pwordChar[index];
@@ -38,7 +42,7 @@ function generatePassword(){
   }
 }
 
-// Prompt user for password criteria
+// Prompt user for password criteria and build array of possible values
 function promptCriteria(){
   if (window.confirm("Will your password need lowercase letters?")){
     pwordChar = pwordChar.concat(alphabetLower);
